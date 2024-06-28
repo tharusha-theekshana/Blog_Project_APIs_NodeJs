20dbdef684796648c062c3762c713213b2708d33
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-06-28 10:56:07 +0530

    Create user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name : {type : String, required : true},
+        email : {type : String, required : true , unique: true , trim : true},
+        password : {type : String, required : true , minLength : 6},
+
+        // 1 -> super admin
+        // 2 -> normal admin
+        // 3 -> normal user
+        role : { type : Number , default : 3}
+    },{ timeStamps : true}
+)
+
+const User = mongoose.model("user",userSchema);
+
+module.exports = User;
