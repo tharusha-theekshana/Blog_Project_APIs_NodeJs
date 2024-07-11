fbb3589b2795e737f45c6ea9cf68a01edc18880d
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-07-11 20:58:38 +0530

    Encrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import hashPassword from "../utils/hashPassword.js";
+
 
 const signup = async (req, res, next) => {
     try {
@@ -11,7 +13,11 @@ const signup = async (req, res, next) => {
             throw new Error("Email already exist.");
         }
 
-        const newUser = new User({name, email, password, role});
+        const hashedPassword = await hashPassword(password);
+        console.log("SSSS" + hashedPassword);
+
+        const newUser = new User({name, email, password : hashedPassword , role});
+        console.log("asdsadas" + newUser );
         await newUser.save();
 
         res.status(200).json({code: 201, status: true, message: "User registered successfully ... !"});
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import {signup} from "../controllers/index.js";
 import {signupValidator,validate} from "../validators/validators.js";
 
-router.post("/signup",signupValidator ,validate , signup);
+router.post("/signup", signupValidator ,validate , signup);
 
 export {router};
