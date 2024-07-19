b2747b0f0adb3ec9204e51de1da09cd121dcdc10
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-07-19 21:59:45 +0530

    Get categories and pagination

--- controllers/category.js
+++ controllers/category.js
@@ -1,48 +1,48 @@
 import Category from "../models/Category.js";
 import User from "../models/User.js";
 
-const addCategory = async (req,res,next) => {
-    try{
-        const {title,desc} = req.body;
+const addCategory = async (req, res, next) => {
+    try {
+        const {title, desc} = req.body;
         const {_id} = req.user;
 
         const isCategoryExist = await Category.findOne({title});
-        if (isCategoryExist){
+        if (isCategoryExist) {
             res.code = 400;
             throw new Error("Category already exist.");
         }
 
         const user = await User.findById(_id);
 
-        if (!user){
+        if (!user) {
             res.code = 404;
             throw new Error("User not found.");
         }
 
-        const newCategory = new Category({title,desc,updatedBy: _id});
+        const newCategory = new Category({title, desc, updatedBy: _id});
         await newCategory.save();
 
         res.status(200).json({code: 200, status: true, message: "Category added successfully."})
 
-    }catch (e){
+    } catch (e) {
         next(e);
     }
 }
 
-const updateCategory = async (req,res,next) => {
-    try{
+const updateCategory = async (req, res, next) => {
+    try {
         const {id} = req.params;
         const {_id} = req.user;
-        const {title,desc} = req.body;
+        const {title, desc} = req.body;
 
         const category = await Category.findById(id);
-        if (!category){
+        if (!category) {
             res.code = 400;
             throw new Error("Category not found.");
         }
 
         const isCategoryExist = await Category.findOne({title});
-        if (isCategoryExist && isCategoryExist.title === title && String(isCategoryExist._id) !== String(category._id)){
+        if (isCategoryExist && isCategoryExist.title === title && String(isCategoryExist._id) !== String(category._id)) {
             res.code = 400;
             throw new Error("Title already exist");
         }
@@ -53,19 +53,19 @@ const updateCategory = async (req,res,next) => {
 
         await category.save();
 
-        res.status(200).json({code: 200, status: true, message: "Category updated successfully.",data : {category}})
+        res.status(200).json({code: 200, status: true, message: "Category updated successfully.", data: {category}})
 
-    }catch (e){
+    } catch (e) {
         next(e);
     }
 }
 
-const deleteCategory = async (req,res,next) => {
-    try{
+const deleteCategory = async (req, res, next) => {
+    try {
         const {id} = req.params;
 
         const category = await Category.findById(id);
-        if (!category){
+        if (!category) {
             res.code = 400;
             throw new Error("Category not found.");
         }
@@ -74,9 +74,46 @@ const deleteCategory = async (req,res,next) => {
 
         res.status(200).json({code: 200, status: true, message: "Category deleted successfully."});
 
-    }catch (e){
+    } catch (e) {
         next(e);
     }
 }
 
-export {addCategory,updateCategory,deleteCategory};
+const getCategory = async (req, res, next) => {
+    try {
+        const {searchQuery, size, page} = req.query;
+        let query = {};
+
+        const sizeNumber = parseInt(size) || 10;
+        const pageNumber = parseInt(page) || 1;
+
+        if (searchQuery) {
+            const search = RegExp(searchQuery, "i");
+            query = {$or: [{title: search}, {desc: search}]};
+        }
+
+        const total = await Category.countDocuments(query).skip((pageNumber - 1) * sizeNumber).limit(sizeNumber).sort({updatedBy: -1});
+        const pages = Math.ceil(total / sizeNumber);
+
+
+        const categories = await Category.find(query);
+
+        if (!categories) {
+            res.code = 400;
+            throw new Error("Categories not found.");
+        }
+
+
+        res.status(200).json({
+            code: 200,
+            status: true,
+            message: "Category search successfully.",
+            data: {categories, total, pages}
+        });
+
+    } catch (e) {
+        next(e);
+    }
+}
+
+export {addCategory, updateCategory, deleteCategory, getCategory}
--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {addCategory, deleteCategory, updateCategory} from "../controllers/category.js";
+import {addCategory, deleteCategory, getCategory, updateCategory} from "../controllers/category.js";
 import {addCategoryValidator, idValidator} from "../validators/categoryValidators.js";
 import validate from "../validators/validateFunction.js";
 import isAuth from "../middlewares/isAuth.js";
@@ -12,5 +12,7 @@ categoryRouter.post("/", isAuth, isAdmin, addCategoryValidator, validate, addCat
 categoryRouter.put("/:id", isAuth, isAdmin, idValidator, addCategoryValidator, validate, updateCategory);
 
 categoryRouter.delete("/:id", isAuth, isAdmin, idValidator, validate, deleteCategory);
+
+categoryRouter.get("/",isAuth,getCategory);
 
 export {categoryRouter};
