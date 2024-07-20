b9a2f2c0971f74a7b3119f9db9c7e88059adb88e
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-07-20 13:00:22 +0530

    File filter and multiple file upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,28 +3,40 @@ import path from "path";
 import generateCode from "../utils/generateCode.js";
 
 const storage = multer.diskStorage({
-    destination : (req,res,callback) => {
-        callback(null,'./uploads');
+    destination: (req, res, callback) => {
+        callback(null, './uploads');
     },
-    filename: (req,file,callback) => {
+    filename: (req, file, callback) => {
 
         //Original_file_name_12_digit_random_number.ext
         const originalName = file.originalname;
         const extension = path.extname(originalName);
-        const fileName = originalName.replace(extension,"");
+        const fileName = originalName.replace(extension, "");
         const compressFileName = fileName.split(" ").join("_");
         const lowerCaseFileName = compressFileName.toLocaleLowerCase();
         const code = generateCode(12);
         const finalFile = `${lowerCaseFileName}_${code}${extension}`;
 
-        callback(null,finalFile);
+        callback(null, finalFile);
 
     },
 })
 
 //Upload middleware
 const upload = multer({
-    storage
+    storage,
+    fileFilter: (req, file, callback) => {
+
+        let type = file.mimetype;
+
+        if (type === "image/png" || type === "image/jpeg" || type === "image/jpg" || type === "application/pdf") {
+            callback(null, true);
+        } else {
+            callback(new Error("Only .png .jpg .jpeg and .png files are allowed"));
+
+        }
+
+    }
 })
 
 export default upload;
