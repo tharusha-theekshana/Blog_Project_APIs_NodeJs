c56b3099559b84240273397384bfd125c63e40d1
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-08-01 23:11:09 +0530

    Store uploaded file in database

--- controllers/file.js
+++ controllers/file.js
@@ -1,12 +1,13 @@
 import fileExtValidator from "../validators/fileValidator.js";
 import uploadFileToAWS from "../utils/awsS3.js";
+import File from "../models/File.js";
 import path from "path";
 
-const uploadFile = async (req,res,next) => {
-    try{
+const uploadFile = async (req, res, next) => {
+    try {
         const {file} = req;
 
-        if (!file){
+        if (!file) {
             res.code = 400;
             throw new Error("File is not selected.");
         }
@@ -15,21 +16,32 @@ const uploadFile = async (req,res,next) => {
         const ext = path.extname(file.originalname);
         const isValidate = fileExtValidator(ext);
 
-        if (!isValidate){
+        if (!isValidate) {
             res.code = 400;
             throw new Error("Only .png .jpg .jpeg and .png files are allowed.");
         }
 
-        const key = await uploadFileToAWS({file,ext});
+        const key = await uploadFileToAWS({file, ext});
+
+        if (key) {
+            const newFile = new File({
+                key,
+                size: file.size,
+                mimetype: file.mimetype,
+                createdBy: req.user
+            })
+
+            await newFile.save();
+        }
 
         res.status(200).json({
-            code : 201,
-            status:true,
+            code: 201,
+            status: true,
             message: "File uploaded.",
             data: {key}
         })
 
-    }catch (e){
+    } catch (e) {
         next(e);
     }
 }
