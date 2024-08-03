15876cf3295c926f734ab4b7c2f32a07841a234c
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-08-03 18:37:18 +0530

    Get posts

--- controllers/post.js
+++ controllers/post.js
@@ -107,5 +107,36 @@ const deletePost = async (req, res, next) => {
     }
 };
 
+const getPosts = async (req, res, next) => {
+    try {
 
-export {addPost, updatePost, deletePost};
+        const {page,size,q} = req.query;
+
+        const pageNumber = parseInt(page) || 1;
+        const sizeNumber = parseInt(page) || 10;
+        let query = {};
+
+        if (q){
+            const search = new RegExp(q,"i");
+
+            query = {
+                $or :[{title:search}]
+            }
+        }
+
+        const total = await Post.countDocuments(query);
+        const pages = Math.ceil(total / sizeNumber);
+
+        const posts = await Post.find(query).sort({updatedBy: -1}).skip((pageNumber -1) * sizeNumber).limit(sizeNumber);
+
+        res
+            .status(201)
+            .json({code: 201, status: true, message: "Get post list successfully" , data : {posts , total , pages }});
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export {addPost, updatePost, deletePost, getPosts};
--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import {addPost, deletePost, updatePost} from "../controllers/post.js";
+import {addPost, deletePost, getPosts, updatePost} from "../controllers/post.js";
 import {addPostValidator, idValidator, updatePostValidator} from "../validators/postValidator.js";
 import validate from "../validators/validateFunction.js";
 
@@ -12,5 +12,7 @@ postRouter.post("/", isAuth, addPostValidator, validate, addPost);
 postRouter.put("/:id", isAuth, updatePostValidator, idValidator, validate, updatePost)
 
 postRouter.delete("/:id", isAuth, idValidator, validate, deletePost)
+
+postRouter.get("/", isAuth, idValidator, validate, getPosts)
 
 export {postRouter};
